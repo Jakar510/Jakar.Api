0c17dc3281780a8538bc5016fd8482c3e8fbd38c
Tyler Stegmaier <jakar510@gmail.com>
2021-04-05 10:02:19 -0500

    added overloads for some iOS layout extensions. 1.2.1

---
+++ src/Jakar.Api.iOS/Enumerations/LayoutPriority.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable 1591
+
+#nullable enable
+namespace Jakar.Api.iOS.Enumerations
+{
+	public enum LayoutPriority // iOS only
+	{
+		Zero = 0,           // 0
+		Minimum = 1,        // 1
+		Lowest = 50,        // UILayoutPriority.FittingSizeLevel,
+		VeryLow = 100,
+		Low = 250,          // UILayoutPriority.DefaultLow,                              
+		BelowAverage = 490, // UILayoutPriority.DragThatCannotResizeScene; 
+		Average = 500,      // UILayoutPriority.SceneSizeStayPut;        
+		AboveAverage = 510, // UILayoutPriority.DragThatCanResizeScene;
+		High = 750,         // UILayoutPriority.DefaultHigh;                           
+		Highest = 999,      // 999
+		Required = 1000,    //  UILayoutPriority.Required; 
+	}
+}
--- src/Jakar.Api.iOS/Extensions/LayoutExtensions.Priorities.cs
+++ src/Jakar.Api.iOS/Extensions/LayoutExtensions.Priorities.cs
@@ -1,4 +1,7 @@
-﻿using UIKit;
+﻿using System;
+using Jakar.Api.Extensions;
+using Jakar.Api.iOS.Enumerations;
+using UIKit;
 
 
 #pragma warning disable 1591
@@ -8,6 +11,8 @@ namespace Jakar.Api.iOS.Extensions
 {
 	public static partial class LayoutExtensions
 	{
+		public static void Priorities( this UIView view, in LayoutPriority hugging, in LayoutPriority compression ) => view.Priorities(hugging.ToFloat(), compression.ToFloat());
+
 		public static void Priorities( this UIView view, in float hugging, in float compression )
 		{
 			view.HuggingPriority(hugging, UILayoutConstraintAxis.Horizontal, UILayoutConstraintAxis.Vertical);
@@ -15,14 +20,32 @@ namespace Jakar.Api.iOS.Extensions
 			view.CompressionPriorities(compression, UILayoutConstraintAxis.Horizontal, UILayoutConstraintAxis.Vertical);
 		}
 
+
+		public static void CompressionPriorities( this UIView view, in LayoutPriority value, params UILayoutConstraintAxis[] directions ) => view.CompressionPriorities(value.ToFloat(), directions);
+
 		public static void CompressionPriorities( this UIView view, in float value, params UILayoutConstraintAxis[] directions )
 		{
-			foreach ( var axis in directions ) { view.SetContentCompressionResistancePriority(value, axis); }
+			foreach ( UILayoutConstraintAxis axis in directions ) { view.SetContentCompressionResistancePriority(value, axis); }
 		}
+
+
+		public static void HuggingPriority( this UIView view, in LayoutPriority value, params UILayoutConstraintAxis[] directions ) => view.HuggingPriority(value.ToFloat(), directions);
 
 		public static void HuggingPriority( this UIView view, in float value, params UILayoutConstraintAxis[] directions )
 		{
-			foreach ( var axis in directions ) { view.SetContentHuggingPriority(value, axis); }
+			foreach ( UILayoutConstraintAxis axis in directions ) { view.SetContentHuggingPriority(value, axis); }
 		}
+
+		public static LayoutPriority ToLayoutPriority( this UILayoutPriority priority ) => priority switch
+																						   {
+																							   UILayoutPriority.Required => LayoutPriority.Required,
+																							   UILayoutPriority.DefaultHigh => LayoutPriority.High,
+																							   UILayoutPriority.DefaultLow => LayoutPriority.Low,
+																							   UILayoutPriority.FittingSizeLevel => LayoutPriority.Lowest,
+																							   UILayoutPriority.DragThatCanResizeScene => LayoutPriority.AboveAverage,
+																							   UILayoutPriority.SceneSizeStayPut => LayoutPriority.Average,
+																							   UILayoutPriority.DragThatCannotResizeScene => LayoutPriority.BelowAverage,
+																							   _ => throw new ArgumentOutOfRangeException(nameof(priority), priority, null)
+																						   };
 	}
 }
