a3fb5cbc8b9e06af3a055ed013c368e3c6b50ef6
Tyler Stegmaier <jakar510@gmail.com>
2021-04-05 11:56:20 -0500

    1.2.2 fix bug in IsOneOf method

--- src/Jakar.Api/Extensions/Extensions.cs
+++ src/Jakar.Api/Extensions/Extensions.cs
@@ -10,12 +10,7 @@ namespace Jakar.Api.Extensions
 	[Xamarin.Forms.Internals.Preserve(true, false)]
 	public static class Extensions
 	{
-		public static bool IsEqual<TValue>( this TValue value, TValue other ) => ( value ?? throw new NullReferenceException(nameof(value)) ).Equals(other);
-		public static bool IsOneOf<TValue>( this TValue value, params TValue[] items ) where TValue : class => items.Any(value.IsEqual);
-
-
-		// public static bool IsOneOfType<TValue>( this TValue value, params object[] items ) where TValue : class => IsOneOfType(value, items.Select(x => x.GetType()).ToArray());
-		public static bool IsOneOf<TValue>( this TValue value, params Type[] items ) where TValue : class => items.Any(value.IsEqual);
-		public static bool IsEqual<TValue>( this TValue value, Type other ) where TValue : class => value.GetType() == other;
+		public static bool IsEqual<TValue>( this TValue value, TValue other ) => Equals(value, other);
+		public static bool IsOneOf<TValue>( this TValue value, params TValue[] items ) => items.Any(other => value.IsEqual(other));
 	}
 }
