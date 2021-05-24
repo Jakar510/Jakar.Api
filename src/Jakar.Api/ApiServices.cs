2f4a723a933c9baec86179b26e29d83e12496c72
Tyler Stegmaier <jakar510@gmail.com>
2021-05-24 09:56:59 -0500

    3.7.0 Updated ApiServices. AccountManager is now optional and in an overload.

--- src/Jakar.Api/ApiServices.cs
+++ src/Jakar.Api/ApiServices.cs
@@ -12,22 +12,17 @@ namespace Jakar.Api
 {
 	public abstract class ApiServices<TDebug,
 									  TPrompts,
-									  TAccounts,
-									  TUser,
 									  TAppSettings,
 									  TFileSystem,
 									  TLanguage,
 									  TDeviceID,
 									  TViewPage> where TDebug : Debug<TDeviceID, TViewPage>, new()
 												 where TPrompts : Prompts<TDeviceID, TViewPage>, new()
-												 where TAccounts : AccountManager<TUser>
-												 where TUser : class, IUser<TUser>, new()
 												 where TAppSettings : IAppSettings<TDeviceID, TViewPage>, new()
 												 where TFileSystem : BaseFileSystemApi, new()
 												 where TLanguage : LanguageApi, new()
 	{
 		public TDebug                         Debug      { get; } = new();
-		public TAccounts                      Accounts   { get; }
 		public TPrompts                       Prompts    { get; } = new();
 		public TAppSettings                   Settings   { get; } = new();
 		public TLanguage                      Language   { get; } = new();
@@ -44,7 +39,6 @@ namespace Jakar.Api
 		/// <param name="appCenterServices"></param>
 		protected ApiServices( in string app_center_id, params Type[] appCenterServices )
 		{
-			Accounts = InstanceCreator.Create<TAccounts>(FileSystem);
 			Prompts.Init(Debug);
 			Prompts.Init(Settings);
 			Debug.Init(FileSystem, Settings, app_center_id, appCenterServices);
@@ -57,22 +51,18 @@ namespace Jakar.Api
 
 	public abstract class ApiServices<TDebug,
 									  TPrompts,
-									  TAccounts,
-									  TUser,
 									  TAppSettings,
 									  TFileSystem,
 									  TLanguage,
 									  TResourceManager,
 									  TDeviceID,
 									  TViewPage> :
-		ApiServices<TDebug, TPrompts, TAccounts, TUser, TAppSettings, TFileSystem, TLanguage, TDeviceID, TViewPage> where TDebug : Debug<TDeviceID, TViewPage>, new()
-																													where TPrompts : Prompts<TDeviceID, TViewPage>, new()
-																													where TAccounts : AccountManager<TUser>
-																													where TUser : class, IUser<TUser>, new()
-																													where TAppSettings : IAppSettings<TDeviceID, TViewPage>, new()
-																													where TFileSystem : BaseFileSystemApi, new()
-																													where TLanguage : LanguageApi, new()
-																													where TResourceManager : BaseResourceDictionaryManager, new()
+		ApiServices<TDebug, TPrompts, TAppSettings, TFileSystem, TLanguage, TDeviceID, TViewPage> where TDebug : Debug<TDeviceID, TViewPage>, new()
+																								  where TPrompts : Prompts<TDeviceID, TViewPage>, new()
+																								  where TAppSettings : IAppSettings<TDeviceID, TViewPage>, new()
+																								  where TFileSystem : BaseFileSystemApi, new()
+																								  where TLanguage : LanguageApi, new()
+																								  where TResourceManager : BaseResourceDictionaryManager, new()
 	{
 		public TResourceManager Resources { get; } = new();
 
@@ -83,5 +73,37 @@ namespace Jakar.Api
 		/// <param name="app_center_id"></param>
 		/// <param name="appCenterServices"></param>
 		protected ApiServices( in string app_center_id, params Type[] appCenterServices ) : base(app_center_id, appCenterServices) { }
+	}
+
+
+
+	public abstract class ApiServices<TDebug,
+									  TPrompts,
+									  TAppSettings,
+									  TAccounts,
+									  TUser,
+									  TFileSystem,
+									  TLanguage,
+									  TResourceManager,
+									  TDeviceID,
+									  TViewPage> :
+		ApiServices<TDebug, TPrompts, TAppSettings, TFileSystem, TLanguage, TResourceManager, TDeviceID, TViewPage> where TDebug : Debug<TDeviceID, TViewPage>, new()
+																													where TPrompts : Prompts<TDeviceID, TViewPage>, new()
+																													where TAppSettings : IAppSettings<TDeviceID, TViewPage>, new()
+																													where TAccounts : AccountManager<TUser>
+																													where TUser : class, IUser<TUser>, new()
+																													where TFileSystem : BaseFileSystemApi, new()
+																													where TLanguage : LanguageApi, new()
+																													where TResourceManager : BaseResourceDictionaryManager, new()
+	{
+		public TAccounts        Accounts  { get; }
+
+
+		/// <summary>
+		/// appCenterServices: pass in the types you want to initialize, for example:  typeof(Microsoft.AppCenter.Analytics.Analytics), typeof(Microsoft.AppCenter.Crashes.Crashes)
+		/// </summary>
+		/// <param name="app_center_id"></param>
+		/// <param name="appCenterServices"></param>
+		protected ApiServices( in string app_center_id, params Type[] appCenterServices ) : base(app_center_id, appCenterServices) => Accounts = InstanceCreator.Create<TAccounts>(FileSystem);
 	}
 }
