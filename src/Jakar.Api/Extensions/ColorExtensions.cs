0a99968ef371fadf88cfe13dccc825dde0990d87
Tyler Stegmaier <jakar510@gmail.com>
2021-07-07 10:31:23 -0500

    Add ColorExtensions

---
+++ src/Jakar.Api/Extensions/ColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Jakar.Extensions.General;
+using Xamarin.Forms;
+
+
+#pragma warning disable 1591
+
+#nullable enable
+namespace Jakar.Api.Extensions
+{
+	public static class ColorExtensions
+	{
+		public static Color ToXfColor( this System.Drawing.Color color ) => new(color.R, color.G, color.B, color.A);
+
+		public static System.Drawing.Color ToDrawingColor( this Color color ) => System.Drawing.Color.FromArgb(color.A.ToInt(), color.R.ToInt(), color.G.ToInt(), color.B.ToInt());
+	}
+}
